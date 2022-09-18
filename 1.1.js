bfe7cef73fe9cd5a2ad58eaf463f7f5d867578f1
ayayousef96 <ayayou1205@gmail.com>
2022-09-19 00:59:51 +0300

    1.1

---
+++ 1.1.js
@@ -0,0 +1,18 @@
+ 
+/*variables*/
+/*--string-- */
+let s="Aya";
+/*--number--*/
+let n=5;
+/**object**/
+var p1 = new Object();
+p1.firstName = "Aya";
+p1.lastName  = "Yousef"; 
+/*boolean*/
+let b=true;
+/*null*/
+let N=null;
+/*undefinde */
+var un;
+/*symbol */
+let s1="@";
