3d43b3a5d282716ef42d1a8c2f6845c55265555d
ayayousef96 <ayayou1205@gmail.com>
2022-09-19 01:02:17 +0300

    2.2

---
+++ 2.2.js
@@ -0,0 +1,4 @@
+function tellFortune(jobTitle, location, partnerName, numberOfChildren){
+    s="You will be a " + jobTitle+"in "+location+"married to " +partnerName + "with " + numberOfChildren +"children";
+    return s;
+}
